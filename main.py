ad225545386b378a43d15146712f7cc8a41a8e52
Artuur <artuur.oerlemans@gmail.com>
2022-08-20 20:36:40 +0200

    Added a way of printing sudokus

--- main.py
+++ main.py
@@ -9,8 +9,29 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+def print_sudoku(sudoku):
+    for y in range(0, 9):
+        if y > 0 and y % 3 == 0:
+            print(27*'-')
+        line_str = [str(x) for x in sudoku[y]]
+        print('  '.join(line_str[0: 3]) + " | " + '  '.join(line_str[3: 6]) + " | " + '  '.join(line_str[6: 9]))
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('sudoku-solver')
+    sudoku_1 = [
+        [1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    ]
+
+    print_sudoku(sudoku_1)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
