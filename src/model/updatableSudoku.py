3a81019422a8152326f0ed970d5b748fbbf8ae91
Artuur <artuur.oerlemans@gmail.com>
2022-08-21 22:51:50 +0200

    Added boxes check

--- src/model/updatableSudoku.py
+++ src/model/updatableSudoku.py
@@ -9,8 +9,7 @@ class UpdatableSudoku:
                 self.field_dict[(x, y)] = Field(sudoku_array[y][x])
 
     def is_possible_state(self):
-        #TODO: add boxes check
-        return self.check_columns() and self.check_rows()
+        return self.check_columns() and self.check_rows() and self.check_boxes()
 
     def check_columns(self):
         for x in range(0, 9):
@@ -32,4 +31,22 @@ class UpdatableSudoku:
                         return False
                     else:
                         values_of_row.add(self.field_dict[(x, y)].value)
+        return True
+
+    def check_boxes(self):
+        for column in range(0, 3):
+            for row in range(0, 3):
+                if not self.check_box((column, row)):
+                    return False
+        return True
+
+    def check_box(self, box):
+        values = set()
+        for x in range(box[0] * 3, box[0] * 3 + 3):
+            for y in range(box[1] * 3, box[1] * 3 + 3):
+                if self.field_dict[(x, y)].value != 0:
+                    if self.field_dict[(x, y)].value in values:
+                        return False
+                    else:
+                        values.add(self.field_dict[(x, y)].value)
         return True
